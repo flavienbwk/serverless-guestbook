d731adf544d100802fcb0c246113f0fbd177e537
flavienbwk <flavien@berwick.fr>
2022-07-15 20:43:03 +0200

    Added cloudant instance

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://abf4f671.eu-de.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
